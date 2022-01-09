80a853abce2b1f1833f9fcfbae809b4a3a700c3e
Murat Özdemir <mrtozdmr92@gmail.com>
2022-01-09 19:24:31 +0300

    Squashed commit of the following:
    commit 068d6eecea092cb9700d6424c5fd279d4723fa72
    Author: Murat Özdemir <mrtozdmr92@gmail.com>
    Date:   Sun Jan 9 19:24:03 2022 +0300
    
        Nft Preview Com. Done
    
    commit fde02350786fee40bade016dd317f1680c371753
    Author: Murat Özdemir <mrtozdmr92@gmail.com>
    Date:   Sun Jan 9 17:23:53 2022 +0300
    
        Add Tailwind
    
    commit 4237882a2dc6aa627cf481f3256008055df358c9
    Author: Murat Özdemir <mrtozdmr92@gmail.com>
    Date:   Sun Jan 9 17:11:41 2022 +0300
    
        Parcel init

---
+++ NftPreviewCard-Parcel/README.md
@@ -0,0 +1,20 @@
+## NFT PREVİEW CARD COMPONENT
+
+09/01/2022
+
+![Desktop](./design/desktop-design.jpg)
+
+## Installation
+
+```bash
+npm install
+npm start
+```
+
+## Source
+
+[Frontend Mentor](https://www.frontendmentor.io/challenges/nft-preview-card-component-SbdUL_w0U)
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ NftPreviewCard-Parcel/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryBlue: "#8bacda",
+        primaryCyan: "#00fff7",
+        mainBg: "#0d192b",
+        cardBg: "#14253d",
+        line: "#2f415b",
+      },
+      fontFamily: {
+        outfit: ["'Outfit'", "sans-serif"],
+      },
+    },
+    plugins: [],
+  },
+};
