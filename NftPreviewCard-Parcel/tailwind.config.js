068d6eecea092cb9700d6424c5fd279d4723fa72
Murat Özdemir <mrtozdmr92@gmail.com>
2022-01-09 19:24:03 +0300

    Nft Preview Com. Done

--- NftPreviewCard-Parcel/tailwind.config.js
+++ NftPreviewCard-Parcel/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlue: "#8bacda",
+        primaryCyan: "#00fff7",
+        mainBg: "#0d192b",
+        cardBg: "#14253d",
+        line: "#2f415b",
+      },
+      fontFamily: {
+        outfit: ["'Outfit'", "sans-serif"],
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
